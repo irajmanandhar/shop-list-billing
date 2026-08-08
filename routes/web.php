ecafc8bcff799d043c768338b656a707635f1aa5
irajmanandhar <irajmdhr@gmail.com>
2026-08-08 22:08:20 +0545

    fix: always open dashboard after login

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,15 @@ use App\Http\Controllers\CheckoutController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PosController;
 use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-Route::inertia('/', 'welcome')->name('home');
+Route::get('/', function () {
+    return auth()->check()
+        ? Redirect::route('dashboard')
+        : Inertia::render('welcome');
+})->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
